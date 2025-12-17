5a3e6b6f76d78bb23e37fbb1c1819db544b12462
navinsunar110-netizen <navinsunar110@gmail.com>
2025-12-17 21:52:05 +0545

    Add App component

---
+++ src/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>🚀 JobFi</h1>
+      <p>Decentralized Marketplace for Local Jobs & Orders</p>
+      <p>Built on Blockchain • Earn Nearby</p>
+    </div>
+  );
+}
+
+export default App;
